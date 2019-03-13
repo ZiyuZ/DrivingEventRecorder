b2d4d4678a6ba0a83d88eb423d51aa5d8a1dd4cf
zhzyx <i@zhzyx.me>
2019-03-13 21:35:00 +0800

    优化readConf()

--- utlis.go
+++ utlis.go
@@ -1,38 +1,32 @@
 package main
 
 import (
+	//"encoding/json"
 	"fmt"
+	"github.com/olekukonko/tablewriter"
 	"gopkg.in/ini.v1"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 )
 
 var confPath = "config.ini"
 
-type Config struct {
-	Debug       bool
-	CallBrowser bool
+type config struct {
+	Debug       bool `json:"debug"`
+	CallBrowser bool `json:"call_browser"`
 
-	DatabasePath string
-	PublicPath   string
+	DatabasePath string `json:"database_path"`
+	PublicPath   string `json:"public_path"`
 
-	ServerPort int
-	Log        bool
-	LogPath    string
+	ServerPort int    `json:"server_port"`
+	Log        bool   `json:"log"`
+	LogPath    string `json:"log_path"`
 }
 
-func checkFilePath(in string) string {
-	pathInfo, err := os.Stat(in)
-	if err != nil {
-		fmt.Printf("Invalid file: %v\n", err)
-		os.Exit(1)
-	}
-	if pathInfo.IsDir() {
-		fmt.Printf("Invalid file path: %v\n", in)
-		os.Exit(1)
-	}
-	return in
-}
+//func checkFilePath(in string) string {
+//}
 
 func checkDirPath(in string) string {
 	pathInfo, err := os.Stat(in)
@@ -47,7 +41,7 @@ func checkDirPath(in string) string {
 	return in
 }
 
-func ReadConf() *Config {
+func readConf() *config {
 	cfg, err := ini.LoadSources(ini.LoadOptions{
 		SkipUnrecognizableLines: true,
 	}, confPath)
@@ -57,13 +51,36 @@ func ReadConf() *Config {
 	}
 
 	logFlag := cfg.Section("server").Key("log").MustBool(false)
-	c := &Config{
-		Debug:        cfg.Section("dev").Key("debug").MustBool(false),
-		CallBrowser:  cfg.Section("dev").Key("call_browser").MustBool(true),
-		DatabasePath: cfg.Section("resource").Key("database_path").Validate(checkFilePath),
-		PublicPath:   cfg.Section("resource").Key("public_path").Validate(checkDirPath),
-		ServerPort:   cfg.Section("server").Key("port").MustInt(5000),
-		Log:          logFlag,
+	c := &config{
+		Debug:       cfg.Section("dev").Key("debug").MustBool(false),
+		CallBrowser: cfg.Section("dev").Key("call_browser").MustBool(true),
+		DatabasePath: cfg.Section("resource").Key("database_path").Validate(func(databasePath string) string {
+
+			pathInfo, err := os.Stat(databasePath)
+			if os.IsNotExist(err) {
+				fmt.Printf("Database file not found. Attempting to create database: %v\n", databasePath)
+				executablePath := cfg.Section("database").Key("executable").MustString(
+					filepath.Join(".", "db", "sqlite3"))
+				sqlPath := cfg.Section("database").Key("init_script").MustString(
+					filepath.Join(".", "db", "init_db.sql"))
+				err := exec.Command("cmd", "/c", executablePath, databasePath, "<", sqlPath).Start()
+				if err == nil {
+					return databasePath
+				} else {
+					fmt.Printf("Failed to create database: %v\n", err)
+					os.Exit(1)
+				}
+			} else if err != nil {
+				fmt.Printf("Invalid file: %v\n", err)
+			} else if pathInfo.IsDir() {
+				fmt.Printf("Database path points to a directory: %v\n", databasePath)
+				os.Exit(1)
+			}
+			return databasePath
+		}),
+		PublicPath: cfg.Section("resource").Key("public_path").Validate(checkDirPath),
+		ServerPort: cfg.Section("server").Key("port").MustInt(5000),
+		Log:        logFlag,
 		LogPath: cfg.Section("server").Key("log_path").Validate(func(in string) string {
 			if !logFlag {
 				return ""
@@ -77,7 +94,26 @@ func ReadConf() *Config {
 			return in
 		}),
 	}
-
+	fmt.Println("Configurations read successfully.")
+	data := [][]string{
+		{"Debug", strconv.FormatBool(c.Debug)},
+		{"CallBrowser", strconv.FormatBool(c.CallBrowser)},
+		{"DatabasePath", c.DatabasePath},
+		{"PublicPath", c.PublicPath},
+		{"ServerPort", strconv.Itoa(c.ServerPort)},
+		{"Log", strconv.FormatBool(c.Log)},
+		{"LogPath", c.LogPath},
+	}
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Config Name", "Value"})
+	table.SetHeaderColor(tablewriter.Colors{tablewriter.Bold},
+		tablewriter.Colors{tablewriter.Bold})
+	table.SetColumnColor(tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiBlackColor},
+		tablewriter.Colors{tablewriter.Bold, tablewriter.FgHiRedColor})
+	for _, v := range data {
+		table.Append(v)
+	}
+	table.Render()
 	return c
 }
 
@@ -88,5 +124,5 @@ func callBrowser(ifCallBrowser bool) {
 			return
 		}
 	}
-	fmt.Printf("Please open \"http://localhost:%v\" in your browser.", C.ServerPort)
+	fmt.Printf("Please open \"http://localhost:%v\" in your browser.\n", C.ServerPort)
 }
