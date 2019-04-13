b387e568c5c8b515f9d50fea2d2eae9527c409d9
ZhZYX <i@zhzyx.me>
2019-04-14 00:54:12 +0800

    修复端口无法修改的问题

--- src/config/backendConfig.js
+++ src/config/backendConfig.js
@@ -1,10 +1,10 @@
 export default {
-  backendApi: process.env.NODE_ENV === 'development' ?
-    `http://${window.location.hostname}:5000/api` : `http://${window.location.host}/api`,
+  backendApi: `http://${process.env.NODE_ENV === 'development' ?
+    window.location.hostname + ":5000" : window.location.host}/api`,
   eventApi: "/event",
   eventDefinitionApi: "/event_definition",
   ratingApi: "/rating",
   videoListApi: "/video_list",
-  backendVideoURL: process.env.NODE_ENV === 'development' ?
-    `http://${window.location.hostname}:5000/video` : `http://${window.location.host}/api`,
+  backendVideoURL: `http://${process.env.NODE_ENV === 'development' ?
+    window.location.hostname + ":5000" : window.location.host}/video`,
 };
